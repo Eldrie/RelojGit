0634d8a4760f44657f5f015050b373827ac80ae7
Edgar Manuel García González <manolo161070@hotmail.com>
2014-10-07 07:59:28 -0500

    Prueba con 5 lineas de texto
    string a1 = '#### ';
    string a2 = "#  # ";
    string a3 = "#  # ";
    string a4 = "#  # ";
    string a5 = "#### ";

--- RelojA/RelojA/Program.cs
+++ RelojA/RelojA/Program.cs
@@ -10,6 +10,30 @@ namespace RelojA
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Ingrese los numeros");
+            string numeros = (Console.ReadLine());
+            char c1 = numeros[0];
+            char c2 = numeros[1];
+            char c3 = numeros[2];
+            char c4 = numeros[3];
+            if (c1 == '0')
+            
+                string a1 = '#### ';
+                string a2 = "#  # ";
+                string a3 = "#  # ";
+                string a4 = "#  # ";
+                string a5 = "#### ";
+            
+                Console.WriteLine(numeros);
+                Console.Write(a1);
+
+
+                Console.ReadLine();
+
+
+            
         }
     }
+   
 }
